dd5df56dd992fc5accc4edaa513d0d335092316e
Chris West (Faux) <git@goeswhere.com>
2017-06-02 19:46:18 +0100

    think about some other image types

--- scripts/generate-images/gen_image.sh
+++ scripts/generate-images/gen_image.sh
@@ -1,26 +1,32 @@
 #!/bin/sh
 set -eu
 
+run="$1"
+output_name="$2"
+size="$3"
+opts="$4"
+
+
 T=$(mktemp -p .)
 D=$(mktemp -d .mounting.XXXXXXXXX)
 trap 'rm "${T}"; rm -r "$D"' EXIT
 
-truncate -s 8M "$T"
+truncate -s "$size" "$T"
 
 # create a single partition that's as big as it can be:
 printf 'n\n\n\n\n\nw\n' | fdisk "$T"
 
 L=$(sudo losetup -P -f --show "$T")
-sudo mkfs.ext4 -O '^64bit' "${L}p1"
+sudo mkfs.ext4 -O "$opts" "${L}p1"
 sudo mount "${L}p1" "$D"
 
 H=$(pwd)
-(cd "$D" && sudo "$H/$1")
+(cd "$D" && sudo "$H/$run")
 
 
 sudo umount "$D"
 rm -r "$D"
 sudo losetup -d "$L"
-cp --sparse=always "$T" "$2"
+cp --sparse=always "$T" "$output_name"
 rm "$T"
 trap '' EXIT
